06a296bf24b6bcaa7ed31d3f5d1efccdd0ac1822
user <alejandro79861@gmail.com>
2025-01-03 11:44:39 -0500

    style: improve component styling and layout consistency across various modules

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,10 +8,10 @@ export default {
 			black: '#000000',
 			primary: '#F1C900',
 			Text1: '#CCCCCC',
-			Text2: '#909199',
+			Text2: '#A8A8A8',
 			colorBg: '#1A1C29',
-			colorBg2: '#2A2D42',
-			stroke: '#2A2D42'
+			colorBg2: '#272A3D',
+			stroke: '#232531'
 		},
 		extend: {},
 	},
